dd432f920de2375a798383014c563a3d76472021
Josh Hawthorne <JoHawth31@gmail.com>
2017-10-20 15:55:57 -0700

    Started maps

--- 05_maps/main.go
+++ 05_maps/main.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+
+	//colors := make(map[string]string)
+
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#4bf745",
+		"blue":  "#4ff123",
+	}
+
+	//Add a value
+	colors["white"] = "#ffffff"
+
+	//Delete a value
+	delete(colors, "white")
+
+	fmt.Println(colors)
+}

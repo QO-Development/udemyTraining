ad1c1c40b468b8f0b01f730d3cba09fe9d7817d1
Josh Hawthorne <JoHawth31@gmail.com>
2017-10-24 16:00:40 -0700

    Added code for direct output

--- 07_http/main.go
+++ 07_http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -13,12 +14,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	//Need to initialize byte slice so its big enough to fit data. Otherwise read function will stop because it thinks byte slice is full
-	bs := make([]byte, 99999)
+	// //Need to initialize byte slice so its big enough to fit data. Otherwise read function will stop because it thinks byte slice is full
+	// bs := make([]byte, 99999)
 
-	i, er := resp.Body.Read(bs)
+	// i, er := resp.Body.Read(bs)
 
-	fmt.Println(i)
-	fmt.Println(er)
-	fmt.Println(string(bs))
+	// fmt.Println(i)
+	// fmt.Println(er)
+	// fmt.Println(string(bs))
+
+	//Does the same as the above commented out code
+	io.Copy(os.Stdout, resp.Body)
+	fmt.Println("")
 }

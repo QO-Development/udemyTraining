010172f08d49d1fd661ddab582f28a570c672c9e
Josh Hawthorne <JoHawth31@gmail.com>
2017-10-24 14:32:58 -0700

    Added code for http networking

---
+++ 07_http/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://www.google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	//Need to initialize byte slice so its big enough to fit data. Otherwise read function will stop because it thinks byte slice is full
+	bs := make([]byte, 99999)
+
+	i, er := resp.Body.Read(bs)
+
+	fmt.Println(i)
+	fmt.Println(er)
+	fmt.Println(string(bs))
+}
